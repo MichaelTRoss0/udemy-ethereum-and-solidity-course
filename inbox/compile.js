c7375d1010444b702eeb56fc23c6c4f31eec3479
Michael Ross <58342654+MichaelTRoss0@users.noreply.github.com>
2021-05-13 13:00:30 -0400

    Created a compile script for the contract

--- inbox/compile.js
+++ inbox/compile.js
@@ -1,4 +1,9 @@
 const path = require("path");
 const fs = require("fs");
+const solc = require("solc");
+// const { SSL_OP_CIPHER_SERVER_PREFERENCE } = require("constants");
 
-const inboxPath = path.resolve(__dirname);
+const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
+const source = fs.readFileSync(inboxPath, "utf8");
+
+module.exports = solc.compile(source, 1).contracts[":Inbox"];
