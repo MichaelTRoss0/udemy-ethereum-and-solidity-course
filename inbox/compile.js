51b924315cab0ba2caaf734867a7d278a45b4c85
Michael Ross <58342654+MichaelTRoss0@users.noreply.github.com>
2021-05-17 13:37:02 -0400

    Wrote a test for the Inbox contract

--- inbox/compile.js
+++ inbox/compile.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const fs = require("fs");
 const solc = require("solc");
-// const { SSL_OP_CIPHER_SERVER_PREFERENCE } = require("constants");
 
 const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
 const source = fs.readFileSync(inboxPath, "utf8");
