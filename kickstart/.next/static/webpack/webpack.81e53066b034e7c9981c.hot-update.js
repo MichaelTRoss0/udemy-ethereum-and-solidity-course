98923f0780282a30ce7ba089a5eefcee183fdebb
Michael Ross <58342654+MichaelTRoss0@users.noreply.github.com>
2021-06-23 13:46:54 -0400

    The landing page now receives a list of all campaigns, regardless of how the page is loaded

---
+++ kickstart/.next/static/webpack/webpack.81e53066b034e7c9981c.hot-update.js
@@ -0,0 +1,12 @@
+self["webpackHotUpdate_N_E"]("webpack",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "712c5828ed3dc06b4fa9"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FL3dlYnBhY2svcnVudGltZS9nZXRGdWxsSGFzaCJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7VUFBQSxvQ0FBb0MsK0JBQStCLEUiLCJmaWxlIjoic3RhdGljL3dlYnBhY2svd2VicGFjay44MWU1MzA2NmIwMzRlN2M5OTgxYy5ob3QtdXBkYXRlLmpzIiwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gZnVuY3Rpb24oKSB7IHJldHVybiBcIjcxMmM1ODI4ZWQzZGMwNmI0ZmE5XCI7IH0iXSwic291cmNlUm9vdCI6IiJ9
