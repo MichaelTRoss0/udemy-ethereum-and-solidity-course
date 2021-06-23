fa64738fa896a6b8409610fb288b54040e76d856
Michael Ross <58342654+MichaelTRoss0@users.noreply.github.com>
2021-06-23 12:03:12 -0400

    Added a Next.js page with web3 functionality

---
+++ kickstart/ethereum/factory.js
@@ -0,0 +1,9 @@
+import web3 from "./web3";
+import CampaignFactory from "./buid/CampaignFactory.json";
+
+const instance = new web3.eth.Contract(
+  JSON.parse(CampaignFactory.interface),
+  "0x207dfae17284f5e440A102127Fbf0bb2538bB8ab"
+);
+
+export default instance;
